a36817b2c9704e951f43bcb2306891e69138deec
YutoImata <yuto.imata.wine@gmail.com>
2025-12-02 15:07:38 +0900

    StartWindowのコメントアウトを解除

--- Assets/TechC/VBattle/Scripts/Select/StartWindow.cs
+++ Assets/TechC/VBattle/Scripts/Select/StartWindow.cs
@@ -3,70 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using TechC.VBattle.Core;
 using TechC.VBattle.Core.Managers;
+using TechC.VBattle.Core.Window;
 using UnityEngine;
-// using Windows.Win32.Foundation;
+using Windows.Win32.Foundation;
 
-namespace TechC.Select
+namespace TechC.VBattle.Select
 {
     /// <summary>
     /// セレクト画面で2キャラ選択したときにStart？をウィンドウを使ったデザインをだす
     /// </summary>
     public class StartWindow : Singleton<StartWindow>
     {
-        // [SerializeField, ReadOnly] private string appearChars = "Start?";
-        // private string[] chars;//appearCharsが一文字づつ入る
-        // private int windowCount;
-        // private bool isAnimating = false;
-        // private List<NativeWindow> windows = new List<NativeWindow>();
+        [SerializeField, ReadOnly] private string appearChars = "Start?";
+        private string[] chars;//appearCharsが一文字づつ入る
+        private int windowCount;
+        private bool isAnimating = false;
+        private List<NativeWindow> windows = new List<NativeWindow>();
 
-        // #region  それぞれの文字のWindow設定
-        // [SerializeField] private List<Vector2> windowTop;//ウィンドウの位置（原点は左上）
-        // [SerializeField] private List<int> windowHeight;
-        // [SerializeField] private List<int> windowWidth;
-        // [SerializeField] private List<Sprite> windowImage;
-        // private List<(NativeWindow,string, Vector2, int, int, Sprite)> windowSettings=new List<(NativeWindow, string, Vector2, int, int, Sprite)>();
-        // #endregion
+        #region  それぞれの文字のWindow設定
+        [SerializeField] private List<Vector2> windowTop;//ウィンドウの位置（原点は左上）
+        [SerializeField] private List<int> windowHeight;
+        [SerializeField] private List<int> windowWidth;
+        [SerializeField] private List<Sprite> windowImage;
+        private List<(NativeWindow,string, Vector2, int, int, Sprite)> windowSettings=new List<(NativeWindow, string, Vector2, int, int, Sprite)>();
+        #endregion
 
-        // protected override bool UseDontDestroyOnLoad => false;
+        protected override bool UseDontDestroyOnLoad => false;
 
-        // private void Start()
-        // {
-        //     var count = appearChars.Length;
-        //     windowCount = count;
-        //     chars = new string[count];
-        //     chars = appearChars.Select(c => c.ToString()).ToArray();
-        //     for (var i = 0; i < windowCount; i++)
-        //     {
-        //         var window = WindowFactory.I.GetWindow(WindowFactory.WindowType.Image);
-        //         windows.Add(window);
-        //         WindowUtility.ResizeWindow((HWND)window.Hwnd, 0, 0);
-        //         WindowUtility.MoveWindow((HWND)window.Hwnd, -1920, -1080);//初期値を画面外に
-        //         var setting = (window, chars[i], windowTop[i], windowHeight[i], windowWidth[i], windowImage[i]);
-        //         windowSettings.Add(setting);
-        //     }
-        //     // ShowStartWindow();
-        // }
+        private void Start()
+        {
+            var count = appearChars.Length;
+            windowCount = count;
+            chars = new string[count];
+            chars = appearChars.Select(c => c.ToString()).ToArray();
+            for (var i = 0; i < windowCount; i++)
+            {
+                var window = WindowFactory.I.GetWindow(WindowFactory.WindowType.Image);
+                windows.Add(window);
+                WindowUtility.ResizeWindow((HWND)window.Hwnd, 0, 0);
+                WindowUtility.MoveWindow((HWND)window.Hwnd, -1920, -1080);//初期値を画面外に
+                var setting = (window, chars[i], windowTop[i], windowHeight[i], windowWidth[i], windowImage[i]);
+                windowSettings.Add(setting);
+            }
+            // ShowStartWindow();
+        }
 
-        // private void Update()
-        // {
-        //     if (!isAnimating) return;
-        //     foreach (var (w, ch, top, height, width, sp) in windowSettings)
-        //     {
-        //         var imageWindow = (ImageWindow)w;
-        //         imageWindow.SetImage(sp.texture);
-        //         WindowUtility.MoveWindow((HWND)w.Hwnd, (int)top.x, (int)top.y);
-        //         WindowUtility.AnimateResizeWindow(w.Hwnd, width, height);
-        //     }
-        // }
+        private void Update()
+        {
+            if (!isAnimating) return;
+            foreach (var (w, ch, top, height, width, sp) in windowSettings)
+            {
+                var imageWindow = (ImageWindow)w;
+                imageWindow.SetImage(sp.texture);
+                WindowUtility.MoveWindow((HWND)w.Hwnd, (int)top.x, (int)top.y);
+                WindowUtility.AnimateResizeWindow(w.Hwnd, width, height);
+            }
+        }
 
-        // public void ShowStartWindow()
-        // {
-        //     isAnimating = true;
-        // }
+        public void ShowStartWindow()
+        {
+            isAnimating = true;
+        }
 
-        // public void HideStartWindow()
-        // {
+        public void HideStartWindow()
+        {
 
-        // }
+        }
     }
 }
