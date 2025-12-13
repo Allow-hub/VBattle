61c4b22879e6358e483bca03d4585b7d767097d1
YutoImata <yuto.imata.wine@gmail.com>
2025-12-13 15:00:29 +0900

    カメラエフェクトのインターフェースを追加

---
+++ Assets/TechC/VBattle/Scripts/InGame/Camera/ICameraEffect.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace TechC.VBattle.InGame.Camera
+{
+    /// <summary>
+    /// カメラエフェクトの共通インターフェース
+    /// </summary>
+    public interface ICameraEffect
+    {
+        /// <summary>
+        /// エフェクトを初期化する
+        /// </summary>
+        /// <param name="cameraTransform">対象のカメラTransform</param>
+        void Initialize(Transform cameraTransform);
+
+        /// <summary>
+        /// エフェクトの現在の状態
+        /// </summary>
+        CameraEffectState State { get; }
+
+        /// <summary>
+        /// エフェクトを適用する
+        /// </summary>
+        /// <param name="intensity">エフェクトの強度</param>
+        /// <param name="duration">エフェクトの継続時間</param>
+        void Apply(float intensity, float duration);
+
+        /// <summary>
+        /// エフェクトを停止する
+        /// </summary>
+        void Stop();
+
+        /// <summary>
+        /// エフェクトをリセットして初期状態に戻す
+        /// </summary>
+        void Reset();
+    }
+}
