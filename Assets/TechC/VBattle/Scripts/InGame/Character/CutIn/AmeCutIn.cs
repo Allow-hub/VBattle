2755eafadbcab354dca52f383c93b913711f9d97
22RENT-16\student <y24340216@gmail.com>
2026-01-21 19:24:41 +0900

    カットイン作成中

--- Assets/TechC/VBattle/Scripts/InGame/Character/CutIn/AmeCutIn.cs
+++ Assets/TechC/VBattle/Scripts/InGame/Character/CutIn/AmeCutIn.cs
@@ -5,6 +5,7 @@ using Cysharp.Threading.Tasks;
 using TechC.VBattle.Core.Managers;
 using TechC.VBattle.Core.Window;
 using UnityEngine;
+using Windows.Win32.Foundation;
 
 namespace TechC.VBattle.InGame.Character
 {
@@ -24,7 +25,13 @@ namespace TechC.VBattle.InGame.Character
         public async UniTask Play()
         {
             var w = WindowFactory.I.GetWindow(WindowFactory.WindowType.Image);
+            WindowUtility.ResizeWindow((HWND)w.Hwnd, Screen.width, Screen.height);
 
+            if (w is ImageWindow imageWindow)
+            {
+                imageWindow.SetTextureToBitmap(tex.texture);
+                WindowUtility.MoveWindow((HWND)w.Hwnd, 0, -Screen.height);
+            }
             await UniTask.Delay(TimeSpan.FromSeconds(2f));
             OnFinished?.Invoke();
         }
